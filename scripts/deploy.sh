be83de9bae0f69aab06f8da65317441b2a2f43ef
Ryan McHenry <ryanmmchenry@gmail.com>
2018-12-13 10:20:55 -0500

    Adding One Page Love banner

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,9 +18,9 @@ aws s3 rm  s3://dinosaur.design --recursive
 echo -e "UPLOADING NON GZIPPED FILES";
 aws s3 cp ./build s3://dinosaur.design --recursive --exclude "*.js" --exclude "*.css" --exclude "*.html" --cache-control max-age=31536000
 echo -e "UPLOADING AND ENCODING GZIPPED FILES TO WWW DOMAIN BUCKET";
-aws s3 cp s3://dinosaur.design --content-encoding gzip --content-type text/javascript --recursive --exclude "*" --include "*.js" --cache-control max-age=31536000
-aws s3 cp s3://dinosaur.design --content-encoding gzip --content-type text/css --recursive --exclude "*" --include "*.css" --cache-control max-age=31536000
-aws s3 cp s3://dinosaur.design --content-encoding gzip --content-type text/html --recursive --exclude "*" --include "*.html" --cache-control max-age=31536000
+aws s3 cp ./build s3://dinosaur.design --content-encoding gzip --content-type text/javascript --recursive --exclude "*" --include "*.js" --cache-control max-age=31536000
+aws s3 cp ./build s3://dinosaur.design --content-encoding gzip --content-type text/css --recursive --exclude "*" --include "*.css" --cache-control max-age=31536000
+aws s3 cp ./build s3://dinosaur.design --content-encoding gzip --content-type text/html --recursive --exclude "*" --include "*.html" --cache-control max-age=31536000
 echo -e "CLEARING CACHE";
 aws cloudfront create-invalidation --distribution-id E3V346L71EO7S7 --paths "/*"
 
--- src/components/shared/Hero/Hero.jsx
+++ src/components/shared/Hero/Hero.jsx
@@ -7,9 +7,25 @@ import CloudSmallDot from "components/shared/SVGs/CloudSmallDot";
 
 import styles from "components/shared/Hero/Hero.module.css";
 
+const onePageLoveStyle = {
+  display: "inline-block",
+  position: "fixed",
+  left: "0",
+  top: "150px",
+  zIndex: "999999"
+};
+
 const Hero = () => {
   return (
     <section className={styles.root}>
+      <a href="https://onepagelove.com/dinosaur" style={onePageLoveStyle}>
+        <img
+          width="73"
+          height="56"
+          src="https://s3.amazonaws.com/onepagelove/one-page-love-award-retina-left.png"
+          alt="One Page Love Award"
+        />
+      </a>
       <section className={styles.root__content}>
         <Header />
         <CloudLarge className={styles["root__cloud-large"]} />
