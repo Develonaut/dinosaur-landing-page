62ee88934f4f733562b1dcd5e7820551c3c3ae44
Ryan McHenry <ryanmmchenry@gmail.com>
2018-12-13 10:37:29 -0500

    Adding SEO and images

--- src/components/shared/Router.jsx
+++ src/components/shared/Router.jsx
@@ -1,71 +1,53 @@
-import React from 'react';
-import { withRouter } from 'react-router';
-import {
-  Switch,
-  Route,
-} from 'react-router-dom';
-import {
-  baseRouteConfig,
-  getRouteBundles,
-} from 'conf/routes';
+import React from "react";
+import { withRouter } from "react-router";
+import { Switch, Route } from "react-router-dom";
+import { baseRouteConfig, getRouteBundles } from "conf/routes";
 
-import PageHead from 'components/shared/PageHead';
+import PageHead from "components/shared/PageHead";
 
-function Router({
-  history,
-  location,
-  match,
-}) {
+function Router({ history, location, match }) {
   const {
     component: baseRouteComponent,
     props: baseRouteProps = {},
-    path: baseRoutePath = '',
+    path: baseRoutePath = ""
   } = baseRouteConfig;
 
   const routeBundle = getRouteBundles([baseRoutePath]);
   return (
     <React.Fragment>
-      <PageHead />
+      {/* <PageHead /> */}
       <Switch>
-        {
-          routeBundle.map(({
-            path,
-            exact,
-            props = {},
-            component,
-          }) => {
-            return (
-              <Route
-                key={path}
-                path={path}
-                exact={exact}
-                render={() => {
-                  const routeProps = {
-                    history,
-                    location,
-                    match,
-                    ...props,
-                  };
+        {routeBundle.map(({ path, exact, props = {}, component }) => {
+          return (
+            <Route
+              key={path}
+              path={path}
+              exact={exact}
+              render={() => {
+                const routeProps = {
+                  history,
+                  location,
+                  match,
+                  ...props
+                };
 
-                  return (
-                    <React.Fragment>
-                      { React.createElement(component, { ...routeProps })}
-                    </React.Fragment>
-                  );
-                }}
-              />
-            );
-          })
-        }
+                return (
+                  <React.Fragment>
+                    {React.createElement(component, { ...routeProps })}
+                  </React.Fragment>
+                );
+              }}
+            />
+          );
+        })}
         <Route
-          render={
-            () => {
-              return (
-                <React.Fragment>
-                  { React.createElement(baseRouteComponent, baseRouteProps)}
-                </React.Fragment>
-              );
-            }}
+          render={() => {
+            return (
+              <React.Fragment>
+                {React.createElement(baseRouteComponent, baseRouteProps)}
+              </React.Fragment>
+            );
+          }}
         />
       </Switch>
     </React.Fragment>
